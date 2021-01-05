c91254c55949bf21acd652f81362aff2465a5cf4
ikegitsan <ssm.no99@gmail.com>
2021-01-05 22:40:22 +0900

    aaa commit

---
+++ app/assets/javascripts/comment.js
@@ -0,0 +1,36 @@
+const { $ } = require("@rails/ujs");
+
+$(function(){
+  function buildHTML(comment){
+    let html = `<p>
+                  <strong>
+                    <a href=/users/${comment.user_id}>${comment.user_name}</a>:
+                  </strong>
+                  ${comment.text}
+                </p>`
+    return html;
+  }
+  $('#new_comment').on('submit', function(e){
+    e.preventDefault();
+    // console.log(this)
+    let formData = new FormData(this);
+    let url = $(this).attr('action')
+    $.ajax({
+      url: url,
+      type: "POST",
+      data: formData,
+      dataType: 'json',
+      processData: false,
+      contentType: false
+    })
+    .done(function(data){
+      let html = buildHTML(data);
+      $('.comments').append(html);
+      $('.textbox').values('');
+      $('.form__submit').prop('disaled', false);
+    })
+    .fail(function(){
+      alert('error');
+    })
+  })
+});
---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,17 @@
+class CommentsController < ApplicationController
+
+  def create
+    comment = Comment.create(comment_params)
+    respond_to do |format|
+      format.html { redirect_to tweet_path(params[:tweet_id]) }
+      format.json
+    end
+    # redirect_to "/tweets/#{comment.tweet.id}" # コメントと結びつくツイートの詳細画面に遷移する
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:text).merge(user_id: current_user.id, tweet_id: params[:tweet_id])
+  end
+end
